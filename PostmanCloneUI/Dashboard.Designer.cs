3aea150afa62b7d1f15b6ac6ec233e8c38838e7f
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-06-15 15:51:07 +0530

    add: gitignore and solution file, font and text; refactor: Form1 to Dashboard

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostmanCloneUI;
 
-partial class Form1
+partial class Dashboard
 {
     /// <summary>
     ///  Required designer variable.
@@ -31,7 +31,8 @@ partial class Form1
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.Text = "Postman Clone";
+        this.Font = new System.Drawing.Font("Segoe UI", 18F);
     }
 
     #endregion
