4aaaeeb11dbae2fc9cd11b8c611a37216c2ae22a
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2025-06-19 16:17:19 +0530

    style: changed variable names, add: name for components

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PostmanCloneUI;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace PostmanCloneUI;
 
 partial class Dashboard
 {
@@ -6,6 +8,13 @@ partial class Dashboard
     ///  Required designer variable.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
+    private System.Windows.Forms.PictureBox logoPicture;
+    private System.Windows.Forms.Label headingLabel;
+    private System.Windows.Forms.Label methodLabel;
+    private System.Windows.Forms.TextBox urlInput;
+    private System.Windows.Forms.Button sendButton;
+    private System.Windows.Forms.Label responseLabel;
+    private System.Windows.Forms.TextBox responseDataOutput;
 
     /// <summary>
     ///  Clean up any resources being used.
@@ -33,6 +42,73 @@ partial class Dashboard
         this.ClientSize = new System.Drawing.Size(800, 450);
         this.Text = "Postman Clone";
         this.Font = new System.Drawing.Font("Segoe UI", 18F);
+
+        this.logoPicture = new PictureBox();
+        this.logoPicture.Name = "logoPicture";
+        this.logoPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+        this.logoPicture.Location = new Point(0, 0);
+        this.logoPicture.Size = new Size(110, 110);
+        this.logoPicture.Image = Image.FromFile("./PostmanCloneUI/assets/postman-logo.png");
+
+        this.headingLabel = new Label();
+        this.headingLabel.Name = "headingLabel";
+        this.headingLabel.Text = "Postman Clone";
+        this.headingLabel.Font = new Font("Segoe UI", 30);
+        this.headingLabel.AutoSize = false;
+        this.headingLabel.Location = new Point(110, 0);
+        this.headingLabel.Size = new Size(690, 110);
+        this.headingLabel.TabIndex = 0;
+
+        this.methodLabel = new Label();
+        this.methodLabel.Name = "methodLabel";
+        this.methodLabel.Text = "Get";
+        this.methodLabel.Font = new Font("Segoe UI", 16);
+        this.methodLabel.AutoSize = false;
+        this.methodLabel.Location = new Point(0, 110);
+        this.methodLabel.Size = new Size(120, 40);
+
+        this.urlInput = new TextBox
+        {
+            Name = "urlInput",
+            Width = 560,
+            Height = 40,
+            Location = new Point(120, 110),
+        };
+
+        this.sendButton = new Button
+        {
+            Name = "sendButton",
+            Text = "Send",
+            Font = new Font("Segoe UI", 14),
+            AutoSize = false,
+            Location = new Point(680, 110),
+            Size = new Size(120, 40),
+        };
+
+        this.responseLabel = new Label();
+        this.responseLabel.Name = "responseLabel";
+        this.responseLabel.Text = "Response Data";
+        this.responseLabel.Font = new Font("Segoe UI", 16);
+        this.responseLabel.AutoSize = false;
+        this.responseLabel.Location = new Point(0, 150);
+        this.responseLabel.Size = new Size(800, 40);
+
+        this.responseDataOutput = new TextBox();
+        this.responseDataOutput.Name = "responseDataOutput";
+        this.responseDataOutput.Multiline = true;
+        this.responseDataOutput.Location = new Point(5, 195);
+        this.responseDataOutput.Width = 790;
+        this.responseDataOutput.Height = 250;
+        this.responseDataOutput.ReadOnly = true;
+
+        // Add controls to Dashboard
+        this.Controls.Add(this.logoPicture);
+        this.Controls.Add(this.headingLabel);
+        this.Controls.Add(this.methodLabel);
+        this.Controls.Add(this.urlInput);
+        this.Controls.Add(this.sendButton);
+        this.Controls.Add(this.responseLabel);
+        this.Controls.Add(this.responseDataOutput);
     }
 
     #endregion
